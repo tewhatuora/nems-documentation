85fdd251a2036a9533e0844e734bd4ec4f05bb26
Owen Thomas <owen.thomas@integration.works>
2024-06-17 16:50:35 +1200

    Update gitignore and add url for publishing to pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ module.exports = async function createConfig() {
     url: 'https://apistandards.digital.health.nz',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/nems_documentation/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
